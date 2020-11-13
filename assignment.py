6ff171c147d6b633e8a6851111f9642d4eee1bf3
ibitron <71407016+ibitron@users.noreply.github.com>
2020-11-13 12:58:43 -0800

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -1,19 +1,46 @@
-#! python3
+def tempConversion(a):
+    pass
+    b = f - 30 
+    c = b / 2 
+    return c
+x = tempConversion(100)
+print(x)
 
-import math
+def factorPair(b,a):
+    pass
+   answer = [a,b]
+   answer.sort()
 
-def tempConversion():
+   larger = answer[1]
+   smaller = answer[0]
 
-
-def factorPair():
-
+   num = larger / smaller
+   answer = [smaller, num]
+   answer.sort()
+   return answer
 
 
 def cosineLaw():
-
-def toRadians():
+    pass
+    
+def toRadians(a):
+    b = math.pi
+    c = a * b
+    e = c / 180
+    return e
 
 def solution():
+    pass
 
-def quadratic():
+def quadratic(a,b,c):
+    num1 = -1*b+math.sqrt(b**2-4*a*c)
+    den1 = 2*a
+    answer1 = num1/den1
+    num2 = -1*b-math.sqrt(b**2-4*a*c)
+    den2 = 2*a
+    answer2 = num2/den2
+    homem = []
+    homem.append( answer1 )
+    homem.append( answer2 )
 
+    return homem
